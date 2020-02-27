2297d563a146aeb751a19a426b51e23dd5a40ca0
Jaroslav Tóth <jtoth@frinx.io>
2020-02-27 10:48:13 +0100

    Wrapping of job pool to stateful widget
    Signed-off-by: Jaroslav Tóth <jtoth@frinx.io>

--- lib/design/tabs/job_pool_tab.dart
+++ lib/design/tabs/job_pool_tab.dart
@@ -4,27 +4,38 @@ import 'package:frinx_job_pooler/design/tabs/common/job_description_widget.dart'
 import 'package:frinx_job_pooler/design/tabs/common/job_location_widget.dart';
 import 'package:frinx_job_pooler/design/tabs/common/job_title_widget.dart';
 import 'package:frinx_job_pooler/model/job_description.dart';
-import 'package:frinx_job_pooler/utils/global_app_constants.dart';
-
-final List<JobDescription> jobData = <JobDescription>[
-  JobDescription(1, 'Job 1',
-      description: 'This is the first job',
-      location: 'Mlynské nivy 4959/48,\n821 09 Bratislava,\Slovakia',
-      jobCoordinates: JobCoordinates(-3.823216, -38.481700)),
-  JobDescription(2, 'Job 2', description: 'This is the second job'),
-];
 
 class JobPoolTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scrollbar(
-        child: ListView.builder(
-          itemBuilder: (BuildContext context, int index) =>
-              _JobItem(jobData[index]),
-          itemCount: jobData.length,
-        ),
-      ),
+      child: Scrollbar(child: _JobList()),
+    );
+  }
+}
+
+class _JobList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _JobListState();
+  }
+}
+
+class _JobListState extends State<_JobList> {
+  final List<JobDescription> jobData = <JobDescription>[
+    JobDescription(1, 'Job 1',
+        description: 'This is the first job',
+        location: 'Mlynské nivy 4959/48,\n821 09 Bratislava,\Slovakia',
+        jobCoordinates: JobCoordinates(-3.823216, -38.481700)),
+    JobDescription(2, 'Job 2', description: 'This is the second job'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) =>
+          _JobItem(jobData[index]),
+      itemCount: jobData.length,
     );
   }
 }
@@ -41,11 +52,12 @@ class _JobItem extends StatelessWidget {
     return ExpansionTile(
       key: PageStorageKey<JobDescription>(entry),
       title: new JobTitleWidget(entry),
-      children: <Widget>[_buildJobDescriptionWidget(entry)],
+      children: <Widget>[_buildJobDescriptionWidget(context, entry)],
     );
   }
 
-  Widget _buildJobDescriptionWidget(JobDescription jobEntry) {
+  Widget _buildJobDescriptionWidget(
+      BuildContext context, JobDescription jobEntry) {
     final List<Widget> rows = [];
     if (entry.description != null) {
       rows.add(JobDescriptionWidget(jobEntry));
@@ -53,9 +65,15 @@ class _JobItem extends StatelessWidget {
     if (entry.location != null) {
       rows.add(JobLocationWidget(jobEntry));
     }
-    rows.add(JobButton(BUTTON_TITLE, () => {
-        // todo
-      }),
+    rows.add(
+      JobButton(
+        BUTTON_TITLE,
+        () => Scaffold.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Job \'${entry.jobTitle}\' has been accepted'),
+          ),
+        ),
+      ),
     );
 
     return Column(
